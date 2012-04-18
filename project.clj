6abf59a3417edce3ac9c3a6aae350a8ad0303561
number23 <number23.cn@gmail.com>
2012-04-18 15:44:23 +0800

    inc libs version

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
-(defproject de.no.number23/commons-lib "0.0.3"
+(defproject de.no.number23/commons-lib "0.0.4"
   :description "my clojure commons lib"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [javax.mail/mail "1.4.4"]]
-  :dev-dependencies [[codox "0.3.0"]
-                     [lein-clojars "0.7.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [javax.mail/mail "1.4.5"]]
+  :dev-dependencies [[codox "0.6.1"]
+                     [lein-clojars "0.8.0"]]
 
   :main de.no.number23.commons-lib.core
-  :aot [de.no.number23.commons-lib.io
-        de.no.number23.commons-lib.util
-        de.no.number23.commons-lib.mail])
+  :aot :all)
