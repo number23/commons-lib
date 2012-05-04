8906c11083706dc29cc7d40f589a656a8a266cc7
number23 <number23.cn@gmail.com>
2012-05-04 12:37:06 +0800

    add date.clj

---
+++ src/de/no/number23/commons_lib/date.clj
@@ -0,0 +1,14 @@
+(ns de.no.number23.commons-lib.date
+  (:import (java.util Calendar Date)))
+
+(defn get-Monday
+  "SUNDAY is first day of the week, force it"
+  [^Date d]
+  (let [cal (Calendar/getInstance)]
+    (doto cal
+      (.setTime d)
+      (.setFirstDayOfWeek (Calendar/SUNDAY)))
+    (let [dt (- (.get cal Calendar/DAY_OF_WEEK) 2)]
+      (doto cal
+        (.add Calendar/DATE (- dt))
+        (.getTime)))))
