6aa1a22579d148d64bb16a1a77fac6e66dfdca60
number23 <number23.cn@gmail.com>
2012-05-09 14:25:02 +0800

    date.clj: add cdts func

--- src/de/no/number23/commons_lib/date.clj
+++ src/de/no/number23/commons_lib/date.clj
@@ -9,3 +9,8 @@
     (let [dt (- (.get cal Calendar/DAY_OF_WEEK) 2)]
       (.add cal Calendar/DATE (- dt))
       (.getTime cal))))
+
+(defn cdts
+  "return current datetime string: yyyy-MM-dd HH:mm:ss"
+  []
+  (format "%1$tF %1$tT" (Date.)))
