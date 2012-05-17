678b958b4c19c598cd30b3d4cd748258622a6fd3
number23 <number23.cn@gmail.com>
2012-05-18 00:25:07 +0800

    fix project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,10 @@
 (defproject de.no.number23/commons-lib "0.0.4"
   :description "my clojure commons lib"
+  :url "https://github.com/number23/commons-lib"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [javax.mail/mail "1.4.5"]]
   :dev-dependencies [[codox "0.6.1"]
